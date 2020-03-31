bd81de09cb27f7e55c025c9fe1e4bb70c43ac926
lukasz <l.kostrzewa12@gmail.com>
2020-03-31 16:58:39 +0200

    refactor guarded thread

--- cpputils/guarded_thread.hpp
+++ cpputils/guarded_thread.hpp
@@ -8,13 +8,16 @@ namespace cpputils {
 /// \brief The guarded_thread calls join in destructor
 /// Based on The C++ Programming Language, Bjarne Stroustrup, 42.2.4
 ///
-struct guarded_thread : std::thread
-{
-    using std::thread::thread;
-    using std::thread::operator=;
-    ~guarded_thread(){ if(joinable()) join(); }
+struct guarded_thread : std::thread {
+  using std::thread::thread;
+  using std::thread::operator=;
+  ~guarded_thread() {
+    if (joinable()) {
+      join();
+    }
+  }
 };
 
-}
+}  // namespace cpputils
 
-#endif // CPPUTILS_GUARDED_THREAD_H
+#endif  // CPPUTILS_GUARDED_THREAD_H
